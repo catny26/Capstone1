1570dcc88f45c685e0a995c041ad29239c6578a4
Cathy Ngo <cngo26@gmail.com>
2022-06-10 12:51:15 -0400

    day2 bbbbb

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,6 +2,7 @@ package com.techelevator;
 
 import com.techelevator.model.VendingMachine;
 import com.techelevator.view.Menu;
+import com.techelevator.view.PurchaseMenu;
 
 public class VendingMachineCLI {
 
@@ -11,9 +12,13 @@ public class VendingMachineCLI {
 	private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
 
 	private Menu menu;
+	private PurchaseMenu purchaseMenu;
+	private VendingMachine vendingMachine;
+//	private static Menu purchaseMenu;
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
+		purchaseMenu = new PurchaseMenu(menu);
 	}
 
 	public void run() {
@@ -24,7 +29,7 @@ public class VendingMachineCLI {
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
-
+				purchaseMenu.run();
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
 				System.out.println("Your change is: " + VendingMachine.getChange());
 				System.exit(1);
--- src/main/java/com/techelevator/view/PurchaseMenu.java
+++ src/main/java/com/techelevator/view/PurchaseMenu.java
@@ -10,38 +10,50 @@ import java.io.OutputStream;
 
 public class PurchaseMenu {
 
-    private final Menu menu;
+    private Menu menu;
+    private VendingMachine vendingMachine;
+
+//    public PurchaseMenu(Menu menu) {
+//        this.menu = menu;
+//    }
+
+    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+    private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+    private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+    private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
 
     public PurchaseMenu(Menu menu) {
         this.menu = menu;
     }
+//        this.vendingMachine = vendingMachine;
 
-    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
-        private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
-        private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION= "Finish Transaction";
-        private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+//    public PurchaseMenu(Menu menu, VendingMachine vendingMachine) {
+//        this.menu = menu;
+//        this.vendingMachine = vendingMachine;
+//    }
 
-//    public PurchaseMenu(Menu menu) {
-//        super(menu);
 
-            while {
-                String choice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+    public void run() {
+        while (true) {
+            String choice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
 
-                if (choice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
-                    // display money amount
-                } else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
-                    try {
-                        System.out.println(Inventory.getInv());
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                } else if (choice.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION)) {
-                    System.out.println("Your change is: " + VendingMachine.getChange());
-                    System.exit(1);
-                    //exit program
+            if (choice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
+                // display money amount
+            } else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+                try {
+                    System.out.println(Inventory.getInv());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
+            } else if (choice.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION)) {
+                System.out.println("Your change is: " + VendingMachine.getChange());
+                System.exit(1);
+                break;
+                //exit program
             }
         }
+    }
+}
 
 
         
